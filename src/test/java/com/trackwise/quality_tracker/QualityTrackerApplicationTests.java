d9714452296b90c89fe819bd607cc03588540a7b
Ayan Sharma <55046063+Ayan-Sharma-aks@users.noreply.github.com>
2025-05-10 12:46:15 +0530

    QA-101: Init Spring Boot Project

---
+++ src/test/java/com/trackwise/quality_tracker/QualityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackwise.quality_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
